91ac6d67ae882751e078e7bca5e2bbbea4f8e0f3
Dennys Fernandes <94493550+fernandesdennys@users.noreply.github.com>
2025-03-27 19:11:45 -0300

    Resolução do desafio

---
+++ ex.01 Operadores Aritiméticos/ex01.js
@@ -0,0 +1,12 @@
+let PriNumero = prompt('Digite um número');
+PriNumero = parseInt(PriNumero);
+let SegNumero = prompt('Digite outro número');
+SegNumero = parseInt(SegNumero);
+
+alert(`A soma é: ${PriNumero + SegNumero}`);
+alert(`a divisão é: ${PriNumero / SegNumero}`);
+alert(`a multiplicação é: ${PriNumero * SegNumero}`);
+alert(`a subtração é: ${PriNumero - SegNumero}`);
+alert(`o resto é ${PriNumero % SegNumero}`)
+alert('FIM');
+
