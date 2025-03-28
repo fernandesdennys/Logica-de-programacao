012c14a3a1855bb4931ca6ef312dba48fedd5a1c
Dennys Fernandes <94493550+fernandesdennys@users.noreply.github.com>
2025-03-28 12:30:38 -0300

    .

---
+++ exemplo5 ex02 Ex aritiméticos e atribuição/script.js
@@ -0,0 +1,9 @@
+let num1 = 10
+num1 += 5
+num1 *= 5
+num1 += 1
+alert(`O resto da divisão de ${num1} por 6 é: ${(num1 % 6)}
+\n A raiz quadrada do resto de ${num1} é: ${(num1 % 6) ** 2}
+\n A variável é: ${num1}\n A variável dividida por 4 é : ${num1 / 4}
+\n Agora a o valor da variável é ${num1 = 19}`)
+console.log(num1)
