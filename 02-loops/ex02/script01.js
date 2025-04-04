72b6f08facd472f056ed42a5d8859965873b7dd7
Dennys Fernandes <94493550+fernandesdennys@users.noreply.github.com>
2025-04-04 15:57:14 -0300

    Loops com while e do while - Daniel Tapias

--- 02-loops/ex02/script01.js
+++ 02-loops/ex02/script01.js
@@ -1,11 +1,28 @@
-let dado01 = parseFloat(Math.random() * 6 + 1);
-let dado02 = parseFloat(Math.random() * 6 + 1);
-let contador = 0
+let dado01 = parseInt(Math.random() * 6 + 1);
+let dado02 = parseInt(Math.random() * 6 + 1);
+let contador = 1
 
 while (dado01 !== dado02) {
   console.log(dado01, dado02)
   contador++
-  dado01 = parseFloat(Math.random() * 6 + 1);
-  dado02 = parseFloat(Math.random() * 6 + 1);
+  dado01 = parseInt(Math.random() * 6 + 1);
+  dado02 = parseInt(Math.random() * 6 + 1);
 }
+console.log(`Os dados foram jogados ${contador} vezes. E o número igual foi ${dado01}`);++
+console.log("FIM DO PROGRAMA")
+
+console.log("-------------------------------------")
+
+dado01 = parseInt(Math.random() * 6 + 1);
+dado02 = parseInt(Math.random() * 6 + 1);
+contador = 1
+
+do {
+  console.log(dado01, dado02)
+  contador++
+  dado01 = parseInt(Math.random() * 6 + 1);
+  dado02 = parseInt(Math.random() * 6 + 1);
+
+} while (dado01 !== dado02)
 console.log(`Os dados foram jogados ${contador} vezes. E o número igual foi ${dado01}`);
+console.log("FIM DO PROGRAMA")
