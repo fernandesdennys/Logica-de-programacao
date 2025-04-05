d35a17fbdb786fa8c644bc010d2e31e82a761d4a
Dennys Fernandes <94493550+fernandesdennys@users.noreply.github.com>
2025-04-05 12:35:30 -0300

    Ex. Ano bissexto com Daniel Tapias

---
+++ 02-loops/ex07/desafio.js
@@ -0,0 +1,10 @@
+for (let i = 0; i <= 100; i++) {
+  if (i % 10 === 0) {
+    continue
+  }
+  console.log(i)
+  let sorteio = parseInt(Math.random() * 100 + 1)
+  if (i % "sorteio", sorteio === 0) {
+    break
+  }
+}
