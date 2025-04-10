93f76475a27f3acf2af0d2606484770572cd2926
Dennys Fernandes <94493550+fernandesdennys@users.noreply.github.com>
2025-04-10 14:28:08 -0300

    Array - Daniel Tapias

--- 03-arrays/ex04/desafio1/script.js
+++ 03-arrays/ex04/desafio1/script.js
@@ -1,10 +1,4 @@
-const numeros = [1, 3, 6, 9, 12, 15, 18, 21, 24, 27, 30, 33, 36, 39, 41, 44, 47, 50]
-let encontrou = false
-for (let i = 0; i < numeros.length; i++) {
-  let n = numeros[i]
-  if (n[i] === null || n[i] === undefined || n[i] === isNaN(n)) {
-    encontrou = true
-    break
-  }
+const numeros = ["Dennys", "Ricardo", "Fernandes"]
+for (let i = nome.length - 1; i >= 0; i--) {
+ document.write(nomes[i] + "<br>") 
 }
-aler(`Encontrou null, undefined ou NaN? ${encontrou}`)
