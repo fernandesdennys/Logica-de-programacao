82b4d7b2a34e9c50bc9437bac8a056f4f6ebc66f
Dennys Fernandes <94493550+fernandesdennys@users.noreply.github.com>
2025-04-14 17:52:44 -0300

    ListaAlunosArrays - Daniel Tapias

--- 06-arrays_objetos/03-media-alunos01/listadealunos.js
+++ 06-arrays_objetos/03-media-alunos01/listadealunos.js
@@ -1,6 +1,6 @@
 const alunos = [
   { nome: "Dennys", notas: [5, 7, 5, 9] },
-  { nome: "Ricardo", notas: [5, 4, 2, 5] },
+  { nome: "Ricardo", notas: [5, 4, 2, 0] },
   { nome: "Fernandes", notas: [10, 7, 8, 9] },
 ]
 const listaAlunos = document.getElementById("listaAlunos")
@@ -8,13 +8,16 @@ let alunosHTML = ""
 
 for (let i = 0; i < alunos.length; i++) {
   alunosHTML += `<li> ${alunos[i].nome}`
-  alunosHTML += `<ul>`
+  alunosHTML += `<ul>`  
+  let soma = 0
   for (let j = 0; j < alunos[i].notas.length; j++) {
     alunosHTML += `<li> ${alunos[i].notas[j]}`
+    soma += alunos[i].notas[j]    
   }
+  let media = soma / alunos[i].notas.length
+  alunosHTML += `<p>Resultado: ${media}`
   alunosHTML += `</ul> `
   alunosHTML += `</li>`
-
 }
 
 listaAlunos.innerHTML = alunosHTML
